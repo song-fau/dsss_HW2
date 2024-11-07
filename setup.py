961891cd847df7620fcb9d9f8a35ef992ffcd514
song-fau <song.zhang@fau.de>
2024-11-07 23:06:17 +0100

    Add setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'random',
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz=math_quiz.math_quiz:math_quiz',  
+        ],
+    },
+)
